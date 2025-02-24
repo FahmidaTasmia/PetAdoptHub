d7e73bd9c4d04e93fd4a24ac6b45be483f84c636
fahmida76 <63741081+fahmida76@users.noreply.github.com>
2025-02-24 16:17:06 +0600

    load all the pets from API and display and use the daisyUI card for better degine implimentation

--- scripts/main.js
+++ scripts/main.js
@@ -50,6 +50,63 @@ const displayCategories = (categories) => {
     });
 };
 
+//load all pets
+
+const loadAllPets =()=>{
+    fetch('https://openapi.programming-hero.com/api/peddy/pets')
+    .then((res)=>res.json())
+    .then((data)=>displayAllPets(data.pets))
+    .catch((error)=>console.log(error))
+}
+
+//display all pets
+const displayAllPets =(pets)=>{
+    const petContainer = document.getElementById('pet-cards')
+ pets.forEach ((pet)=>{
+    // console.log(pet)
+    const card = document.createElement('div');
+    card.classList="card shadow-xl ";
+    card.innerHTML = `
+    <figure class="px-10 pt-10">
+    <img class="h-full w-full object-cover rounded-xl "   
+            src=${pet.image}
+            alt="Shoes" />
+  </figure>
+  <div class="card-body ">
+    <h2 class="card-title">${pet.pet_name}</h2>
+    <p class=" text-gray-600 flex items-center">
+                    <img src="https://img.icons8.com/?size=100&id=MtZ7t7uMioaM&format=png&color=000000" class="mr-2 w-5" > 
+                   Breed: ${pet.breed}
+                </p>
+                <p class=" text-gray-600 flex items-center">
+                    <img src="https://img.icons8.com/?size=100&id=GlEOr5x0aJpH&format=png&color=000000" class="mr-2 w-5" alt="Calendar Icon"> 
+                    Birth: ${pet.date_of_birth}
+                </p>
+                <p class=" text-gray-600 flex items-center">
+                    <img src="https://img.icons8.com/?size=100&id=Or88pxF6uBMj&format=png&color=000000" class="mr-2 w-5" alt="Gender Icon"> 
+                   Gender: ${pet.gender}
+                </p>
+                <p class=" text-gray-600  flex items-center">
+                    <img src="https://img.icons8.com/?size=100&id=85782&format=png&color=000000" class="mr-2 w-5" alt="Price Icon"> 
+                  price: ${pet.price}
+                </p>
+    <div class="card-actions">
+        <div class ="flex gap-3 ">
+        <button class="btn btn-sm rounded-lg">
+        <img src="https://img.icons8.com/?size=100&id=82788&format=png&color=000000"/ class="w-5">
+        </button>
+        <button class="btn btn-sm text-teal-600 bg-white border border-gray-200 px-3 rounded-lg">Adopt</button>
+        <button class="btn btn-sm text-teal-600 bg-white border border-gray-200 px-3 rounded-lg">Details</button>
+        </div>
+    </div>
+  </div>
+    `
+
+    petContainer.appendChild(card)
+ })
+}
+
 
 
 loadCategories();
+loadAllPets();
